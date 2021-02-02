9ca879ae9b5d7232a83b692c2d5e8fad0caf0660
Johnny-smiles <70188711+Johnny-smiles@users.noreply.github.com>
2021-02-01 23:45:49 -0600

    added queries and resolvers.

---
+++ server/schemas/index.js
@@ -0,0 +1,6 @@
+// importing in type definitions
+const typeDefs = require('./typeDefs');
+// importing in resolvers
+const resolvers = require('./resolvers');
+
+module.exports = { typeDefs, resolvers };
