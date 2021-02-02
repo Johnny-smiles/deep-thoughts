ef6499ea880c51f7b4caccd97dc6cda29b312cfa
Johnny-smiles <70188711+Johnny-smiles@users.noreply.github.com>
2021-02-01 23:50:51 -0600

    snapshot compare

--- server/schemas/index.js
+++ server/schemas/index.js
@@ -1,6 +1,4 @@
-// importing in type definitions
 const typeDefs = require('./typeDefs');
-// importing in resolvers
 const resolvers = require('./resolvers');
 
 module.exports = { typeDefs, resolvers };
